9ea61d98ad11a67e50a2c43e2981ab2cd5d07872
decentralised-project <hello@decentralised-project.org>
2017-04-14 23:37:50 +0100

    added crypt base project files

---
+++ src/decentralised_crypt.cpp
@@ -0,0 +1,6 @@
+#include "decentralised_crypt.h"
+
+
+decentralised_crypt::decentralised_crypt()
+{
+}
---
+++ src/decentralised_crypt.h
@@ -0,0 +1,12 @@
+#ifndef DECENTRALISED_CRYPT_H
+#define DECENTRALISED_CRYPT_H
+
+
+class decentralised_crypt
+{
+
+public:
+    decentralised_crypt();
+};
+
+#endif // DECENTRALISED_CRYPT_H
