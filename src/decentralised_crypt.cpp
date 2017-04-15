846d94f5ca912cc625a4e48a7362a5e962d45787
decentralised-project <hello@decentralised-project.org>
2017-04-15 15:59:49 +0100

    #4 added method for generating new keypair.

--- src/decentralised_crypt.cpp
+++ src/decentralised_crypt.cpp
@@ -1,6 +1,32 @@
 #include "decentralised_crypt.h"
 
 
-decentralised_crypt::decentralised_crypt()
+decentralised_crypt::decentralised_crypt(QObject *parent) : QObject(parent)
 {
+    _eckey = NULL;
+}
+
+decentralised_crypt::~decentralised_crypt()
+{
+    if (_eckey)
+        EC_KEY_free(_eckey);
+
+    EVP_cleanup();
+}
+
+EC_KEY* decentralised_crypt::generate_key_pair()
+{
+    if (_eckey)
+        EC_KEY_free(_eckey);
+
+    _eckey = EC_KEY_new();
+
+    EC_GROUP *ecgroup = EC_GROUP_new_by_curve_name(NID_secp256k1);
+    EC_KEY_set_group(_eckey, ecgroup);
+
+    EC_KEY_generate_key(_eckey);
+
+    EC_GROUP_free(ecgroup);
+
+    return _eckey;
 }
--- src/decentralised_crypt.h
+++ src/decentralised_crypt.h
@@ -1,12 +1,29 @@
 #ifndef DECENTRALISED_CRYPT_H
 #define DECENTRALISED_CRYPT_H
 
+#include <QObject>
+#include <openssl/bn.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+#include <openssl/ecdsa.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/evp.h>
 
-class decentralised_crypt
+class decentralised_crypt: public QObject
 {
-
+    Q_OBJECT
 public:
-    decentralised_crypt();
+    explicit decentralised_crypt(QObject *parent = 0);
+    ~decentralised_crypt();
+
+    EC_KEY* generate_key_pair();
+
+signals:
+
+private:
+    EC_KEY* _eckey;
 };
 
 #endif // DECENTRALISED_CRYPT_H
